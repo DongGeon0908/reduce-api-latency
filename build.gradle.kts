2895e82d468458b89e2b8bba85a9a6553bbc76f7
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-09-17 13:15:19 +0900

    fix: spring mvc(tomcat) -> spring webflux(netty)

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ object DependencyVersion {
 
 dependencies {
     /** spring starter */
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
